2f56000322cf4e9c776aba33e116206ee2d943c9
Motjiang <motjiangbrilliant@gmail.com>
2024-12-23 02:07:59 +0200

    Added a footer and its css

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  email: string = 'info@outside.co.za';
+  phone: string = '041 123 4567';
+  address: string = '123 Marine Drive, Summerstrand, Port Elizabeth, 6001, Eastern Cape, South Africa';
 }
